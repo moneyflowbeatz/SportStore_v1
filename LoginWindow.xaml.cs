255307161b16c5bf9d1be9ca3f771bd1239fcb15
moneyflow <moneyflowbeatz@gmail.com>
2023-11-01 20:05:21 +0300

    Настройка Captcha

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SportStore.Infrastructure;
+using SportStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace SportStore
 {
     /// <summary>
@@ -19,15 +22,87 @@ namespace SportStore
     /// </summary>
     public partial class LoginWindow : Window
     {
+        bool verify = true;
+        int verifyCheck = 0;
         public LoginWindow()
         {
             InitializeComponent();
+
+            captchaBlock.Visibility = Visibility.Collapsed;
+            captchaBox.Visibility = Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// Асинхронное выключение кнопки на 10 сек.
+        /// </summary>
+        async void disableButton()
+        {
+            loginButton.IsEnabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            loginButton.IsEnabled = true;
         }
 
         private void loginButton_Click(object sender, RoutedEventArgs e)
         {
-            new MainWindow().Show();
-            this.Close();
+            using (SportStoreContext db = new SportStoreContext())
+            {
+
+                // проверка, если есть каптча
+                if (captchaBlock.Visibility == Visibility.Visible)
+                {
+                    if (captchaBlock.Text == captchaBox.Text)
+                    {
+                        verify = true;
+                    }
+                }
+
+                User user = db.Users.Where(u => u.Login == loginBox.Text && u.Password == passwordBox.Password).FirstOrDefault() as User;
+
+                // admin
+                if (user != null && verify)
+                {
+                    new MainWindow().Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неуспешная авторизация");
+                    verifyCheck += 1;
+
+                    // captcha view
+                    captchaBox.Visibility = Visibility.Visible;
+                    captchaBlock.Visibility = Visibility.Visible;
+                    captchaBlock.Text = CaptchaBuilder.Refresh();
+                    verify = false;
+
+                    if (verifyCheck > 1)
+                    {
+                        disableButton();
+                        captchaBlock.Text = CaptchaBuilder.Refresh();
+                    }
+                }
+            }
+        }
+    }
+}
+
+namespace SportStore.Infrastructure
+{
+    public static class CaptchaBuilder
+    {
+        public static string Refresh()
+        {
+
+            string captcha = "";
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                captcha += (char)rand.Next('A', 'Z' + 1) + rand.NextInt64(1, 100).ToString();
+            }
+
+            return captcha;
         }
     }
 }
